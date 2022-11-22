bfeb16babc452c78c1d9469ea59d0cbcece674dd
Rodney McGee <rodneymcgee73@gmail.com>
2022-11-22 14:20:14 -0600

    Created Header Component & Added Styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import RegisterPage from "./pages/RegisterPage/RegisterPage";
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
+import HeroImage from "./components/Hero_Image/HeroImage";
 
 // Util Imports
 import PrivateRoute from "./utils/PrivateRoute";
@@ -18,6 +19,7 @@ function App() {
   return (
     <div>
       <Navbar />
+      <HeroImage />
       <Routes>
         <Route
           path="/"
---
+++ frontend/src/components/Hero_Image/HeroImage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./HeroImage.css";
+
+
+const HeroImage = () => {
+    return (
+        <div className='hero-image'>
+            <div className='hero-text'>
+                <h1>Elite Dog Training</h1>
+            </div>
+        </div>
+    );
+}; 
+
+export default HeroImage;
