80cff27a3046a1e0327129c64459765c8033fec6
Rodney McGee <rodneymcgee73@gmail.com>
2022-11-29 10:16:36 -0600

    Added Contact Us Section in Footer Including Phone Number & Email Address

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,6 +3,12 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer>
+      <h4 className="contact-text">Contact Us</h4>
+      <a href="tel:123456789">123456789</a>
+      <p>
+        Email
+      </p>
+      <a href="email">elitetrainers@fake.com</a>
       <p>Copyright © 2022</p>{" "}
     </footer>
   );
