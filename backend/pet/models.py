6f222cec3a58454bc301d230e5a5c1030055828a
Rodney McGee <rodneymcgee73@gmail.com>
2022-12-10 23:30:58 -0600

    Changed User to Owner in Pet Models & Serializers Files + Added Breed To Pet Models & Serializers Files + Made Migration to MySQL Database

--- backend/pet/models.py
+++ backend/pet/models.py
@@ -3,7 +3,8 @@ from authentication.models import User;
 
 # Create your models here.
 class Pet(models.Model):
-    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    owner= models.ForeignKey(User, on_delete=models.CASCADE)
+    breed= models.CharField(max_length=255)
     name= models.CharField(max_length=255)
     age= models.IntegerField()
     temperament= models.CharField(max_length=255)
