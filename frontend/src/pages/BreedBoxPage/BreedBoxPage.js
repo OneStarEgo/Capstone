d88f03246242b2bbd2b4a057c7527547dc4ec1a0
Rodney McGee <rodneymcgee73@gmail.com>
2023-02-05 22:48:09 -0600

    Added Input Fields For Dog Names & Breeds

--- frontend/src/pages/BreedBoxPage/BreedBoxPage.js
+++ frontend/src/pages/BreedBoxPage/BreedBoxPage.js
@@ -1,16 +1,22 @@
 import React, { useState, useRef } from 'react';
 import axios from 'axios';
 import Rate from '../../components/RatingBar/Rating';
+import useAuth from '../../hooks/useAuth';
 
 const BreedBoxPage = (props) => {
     const [dogName, setDogName] = useState([]);
     const [breed, setBreed] = useState([]);
-    const [comment, setComments] = useState([])
+    const [comment, setComments] = useState([]);
+    const [user, token] = useAuth();
 
 
     const postNewComment = async (newComment) => {
         try {
-            let response = await axios.post('http://127.0.0.1:8000/api/comments/', newComment);
+            let response = await axios.post('http://127.0.0.1:8000/api/comments/', newComment, {
+                headers: {
+                    Authorization: " Bearer " + token,
+                },
+            });
             setComments(response.data.items)
         } catch (error) {
             console.log(error.response.data)
@@ -23,6 +29,8 @@ const BreedBoxPage = (props) => {
     function handleSubmit(event){
         event.preventDefault()
         let newComment = {
+            email : user.email,
+            username : user.username,
             dog : dogName,
             breed : breed,
             comment : comment,
@@ -41,11 +49,31 @@ const BreedBoxPage = (props) => {
             <h1> Breed Box Comments </h1>
             <hr />
             <form ref={form}  style={{display: 'flex'}} onSubmit={handleSubmit}>
+                <label>
+                Dog Name:{" "}
+                <input
+                type="text"
+                name="dogName"
+                value={dogName}
+                onChange={(event) => setDogName(event.target.value)}
+                />
+                </label>
+                <label>
+                Breed:{" "}
+                <input
+                type="text"
+                name="breed"
+                value={breed}
+                onChange={(event) => setBreed(event.target.value)}
+                />
+                </label>
+
                 <input type='text'
-                    name='text'
+                    name='comment'
                     style={{width: '100%', borderRadius: '5px'}}
                     defaultValue=''
                     placeholder="write comment here"
+                    value={comment}
                     onChange={handleChange}
                 />
                 <br />
